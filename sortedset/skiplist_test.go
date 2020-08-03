be16dac53cca296a01e78cae65780b88e7975ae1
KingPerson <1994778686s@gmail.com>
2020-08-03 17:44:06 +0800

    add skiplist_test.go,Perfect with Xdebug

--- sortedset/skiplist_test.go
+++ sortedset/skiplist_test.go
@@ -1,1 +1,30 @@
 package sortedset
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestZskiplist_ZslInsert(t *testing.T) {
+	zsl := ZslCreate()
+	node1 := zsl.ZslInsert("song", 100)
+	fmt.Println(node1)
+
+	node2 := zsl.ZslInsert("jia", 200)
+	fmt.Println(node2)
+
+	fmt.Println(zsl)
+
+	zsl.ZslDelete(node1.Ele, node1.Score)
+
+	fmt.Println(node1)
+
+	zsl.ZslDelete(node2.Ele, node2.Score)
+
+	fmt.Println(zsl)
+}
+
+
+func TestZskiplist_ZslDelete(t *testing.T) {
+
+}
